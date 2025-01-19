829262b3982e79f72fcf2d4d5767025f432ff189
Reymondox <78160421+Reymondox@users.noreply.github.com>
2025-01-19 02:14:24 -0400

    Add files via upload

---
+++ CalculadoraArea/Clases/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraArea.Solucion
+{
+    public class Circle : Shape
+    {
+        public double Radio { get; set; }
+        public Circle(double radio) : base(radio, radio) {
+            Radio = radio;
+        }
+
+        public override double CalculateSurface()
+        {
+            return Math.PI * Math.Pow(Anchura, 2);
+        }
+
+        public override void MostrarPropiedades()
+        {
+         Console.WriteLine($"Radio: {Radio}");
+        }
+    }
+}
